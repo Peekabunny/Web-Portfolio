ec2380b0e5cf3bedc56b14ed994f2199bcd8e874
Peekabunny <139279625+Peekabunny@users.noreply.github.com>
2023-12-29 22:47:00 -0500

    sections

--- script.js
+++ script.js
@@ -12,20 +12,24 @@ const themeToggleBtn = document.querySelector('.theme-toggle');
 const theme = localStorage.getItem('theme');
 
 // on mount
-theme && document.body.classList.add('dark-mode');
+theme && document.body.classList.add(theme);
 
 // handlers
 const handleThemeToggle = () => {
   document.body.classList.toggle('dark-mode');
+  var img = document.getElementById("icon");
   if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('theme', 'dark-mode');
-  } 
-  else{
-        localStorage.removeItem('theme')
-    }
+    img.src = '/images/sun.png';
+  } else {
+    img.src = '/images/moon.png';
+    localStorage.removeItem('theme');
+  }
 };
 
-//event
-
-
+// events
 themeToggleBtn.addEventListener('click', handleThemeToggle);
+
+
+
+
