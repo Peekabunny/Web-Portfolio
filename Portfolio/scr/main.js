2815b0661d98f99b5dcd95532c44c995c81fa912
Peekabunny <139279625+Peekabunny@users.noreply.github.com>
2024-01-03 11:32:28 -0500

    changes

--- Portfolio/scr/main.js
+++ Portfolio/scr/main.js
@@ -1,6 +1,7 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/Components/header.css';
+import '../styles/Components/hero.css';
 import '../styles/utils.css';
 
 
