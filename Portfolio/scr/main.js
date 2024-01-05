36eed6f5c9ed19c2d5a1331c751c059dcc527b4a
Vincent Do <139279625+Peekabunny@users.noreply.github.com>
2024-01-04 21:44:50 -0500

    featured section
    finish featured section. begin project section.

--- Portfolio/scr/main.js
+++ Portfolio/scr/main.js
@@ -2,6 +2,8 @@ import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/Components/header.css';
 import '../styles/Components/hero.css';
+import '../styles/Components/about.css';
+import '../styles/Components/featured.css';
 import '../styles/utils.css';
 
 
