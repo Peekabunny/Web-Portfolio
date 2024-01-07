a5953a2cbeebc2710d0f640c15c00719698bcb9c
Vincent Do <139279625+Peekabunny@users.noreply.github.com>
2024-01-06 23:25:34 -0500

    inprogress-mobile-nav

--- Portfolio/scr/main.js
+++ Portfolio/scr/main.js
@@ -1,10 +1,13 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/Components/header.css';
+import '../styles/Components/mobile-nav.css';
 import '../styles/Components/hero.css';
 import '../styles/Components/about.css';
 import '../styles/Components/featured.css';
 import '../styles/Components/work.css';
+import '../styles/Components/contact.css';
+import '../styles/Components/footer.css';
 import '../styles/utils.css';
 
 
