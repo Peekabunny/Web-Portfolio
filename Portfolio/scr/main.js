a617ae6e53d95570ca4de47312e4362106bf0476
Vincent Do <139279625+Peekabunny@users.noreply.github.com>
2024-01-08 08:43:23 -0500

    lazy-loading in-progress

--- Portfolio/scr/main.js
+++ Portfolio/scr/main.js
@@ -1,13 +1,15 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/Components/header.css';
-import '../styles/Components/mobile-nav.css';
 import '../styles/Components/hero.css';
 import '../styles/Components/about.css';
 import '../styles/Components/featured.css';
 import '../styles/Components/work.css';
 import '../styles/Components/contact.css';
 import '../styles/Components/footer.css';
+import '../styles/Components/mobile-nav.css';
 import '../styles/utils.css';
 
+import darkMode from './utils/dark-mode';
 
+darkMode();
